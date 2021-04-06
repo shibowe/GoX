6cfd0837d8160d404455ffa5faf5c2c4f179d00f
shibowe <dev@winecos.com>
2021-04-06 16:23:18 +0800

    nothing changed

--- learning/ch6/array_test/array_test.go
+++ learning/ch6/array_test/array_test.go
@@ -1,9 +1,18 @@
 package array_test
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestArrayInit(t *testing.T) {
 
 	var arr [3]int
 	t.Log(arr[1], arr[2])
+}
+
+func TestArrayTravel(t *testing.T) {
+	arr3 := [...]int{1, 3, 4, 6, 7, 9}
+	for idx, e := range arr3 {
+		t.Log(idx, e)
+	}
 }
